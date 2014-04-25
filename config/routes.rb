ad68ebccdbfc0a6de5c9e6139f1221b23b1baa1a
Daniel Lockhart <daniel@lockyy.com>
2014-04-25 02:20:31 +0100

    404s redirect to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Lockyy::Application.routes.draw do
 
-	#Casein routes
-	namespace :casein do
-		resources :images
-		resources :bios
-		resources :items
-		resources :sections
-	end
+  #Casein routes
+  namespace :casein do
+    resources :images
+    resources :bios
+    resources :items
+    resources :sections
+  end
 
   root "static_pages#bio"
   match '/portfolio',    to: "static_pages#portfolio",      via: 'get'
 
-  
+  unless Rails.env.development?
+  	get '*path' => redirect('/')
+  end
+
 end
