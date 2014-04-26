239d7bc0164b2b3c337e16c1c186afd7ae2dba9a
Daniel Lockhart <daniel@lockyy.com>
2014-04-26 18:53:05 +0100

    Human readable image urls

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,10 +2,17 @@ class Image < ActiveRecord::Base
 
 	belongs_to :item
 
-	has_attached_file :image, styles: { thumb: ["100x100#", :png] }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { thumb: ["100x100#", :png] }, url: :image_url
 
 	validates_attachment :image, 	:presence => true,
 									content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/bmp"] },
 									:size => { :in => 0..5.megabytes }
 
+	validates :image_file_name, uniqueness: true
+
+	private
+
+		def image_url
+			return "/images/:style/#{self.image_file_name}"
+		end
 end
