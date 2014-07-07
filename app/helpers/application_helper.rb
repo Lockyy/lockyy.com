edb7501c7d7384a05effe71e00dce4c9b725a1eb
Daniel Lockhart <daniel@lockyy.com>
2014-07-08 00:13:17 +0100

    Add padding to articles, fix code blocks, and remove fixed header.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
 
   def markdown(blogtext)
-    renderOptions = {hard_wrap: true, filter_html: true}
-    markdownOptions = {autolink: true, no_intra_emphasis: true, fenced_code_blocks: true}
+    renderOptions = { hard_wrap: true, 
+                      filter_html: true }
+
+    markdownOptions = { autolink: true, 
+                        no_intra_emphasis: true, 
+                        fenced_code_blocks: true, 
+                        gh_blockcode: true }
+                        
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(renderOptions), markdownOptions)
     markdown.render(blogtext).html_safe
   end
