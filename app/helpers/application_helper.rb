059363f755d78c2c916a32e73b5f2774ed0634f8
Daniel Lockhart <daniel@lockyy.com>
2014-07-11 14:44:08 +0100

    Add dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,13 @@ module ApplicationHelper
     markdown.render(blogtext).html_safe
   end
 
+  def full_title(page_title)
+    base_title = "Daniel Lockhart"
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
