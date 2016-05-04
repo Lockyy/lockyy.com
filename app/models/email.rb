e2791d203bb8fb21816f9f6ecedba30424aa56ae
Daniel Lockhart <daniel@lockyy.com>
2016-05-04 03:19:21 +0100

    Don't submit an email to mailchimp if it's already subscribbed

--- app/models/email.rb
+++ app/models/email.rb
@@ -9,11 +9,11 @@ class Email < ActiveRecord::Base
 
   def send_to_mailchimp
     mailchimp = Mailchimp::API.new(ENV['MAILCHIMPAPIKEY'])
+    return if mailchimp.lists.members(ENV['MAILCHIMPLISTID'])['data'].map { |email| email['email'] }.include?(email)
     mailchimp.lists.subscribe(ENV['MAILCHIMPLISTID'], {
       "email" => email,
       "euid"  => id,
       "leid"  => id,
     })
-  rescue
   end
 end
