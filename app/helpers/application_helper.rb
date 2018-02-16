b747b8bafa86308b74b7a738c7f2829897b96207
Daniel Lockhart <daniel@lockyy.com>
2018-02-16 23:46:11 +0000

    Improve markdown rendering
    Better styling, provided by https://github.com/sindresorhus/github-markdown-css/blob/gh-pages/github-markdown.css.
    Adds Coderay syntax highlighting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,29 @@
 module ApplicationHelper
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
   def markdown(blogtext)
-    render_options = { hard_wrap: true }
+    render_options = {
+      filter_html: true,
+      hard_wrap:   true,
+    }
+
+    coderayified = CodeRayify.new(render_options)
 
     markdown_options = {
       autolink:           true,
       no_intra_emphasis:  true,
       fenced_code_blocks: true,
       gh_blockcode:       true,
+      strikethrough:      true,
+      lax_html_blocks:    true,
+      superscript:        true,
     }
 
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(render_options), markdown_options)
+    markdown = Redcarpet::Markdown.new(coderayified, markdown_options)
     markdown.render(blogtext).html_safe
   end
 
