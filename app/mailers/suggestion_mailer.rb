2643b53e0f7043f34b06fdf7ba5a67feb5366890
Daniel Lockhart <daniel@lockyy.com>
2018-02-19 01:03:17 +0000

    Use respond_to instead of from for suggestor emails

--- app/mailers/suggestion_mailer.rb
+++ app/mailers/suggestion_mailer.rb
@@ -1,11 +1,9 @@
 class SuggestionMailer < ApplicationMailer
-  default from: 'suggestions@lockyy.com'
-
   def suggestion(email, suggestion)
     @email = email
     @suggestion = suggestion
-    mail(from:    @suggestion.suggester_email,
-         to:      ENV['SUGGESTION_BOX_EMAIL'],
-         subject: 'Suggestion from website')
+    mail(to:         ENV['SUGGESTION_BOX_EMAIL'],
+         subject:    'Suggestion from website',
+         respond_to: 'suggestions@lockyy.com')
   end
 end
