559258ffe36340bb802acfe772ad069808ddea95
Daniel Lockhart <daniel@lockyy.com>
2019-09-20 16:07:56 +0100

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Bio.create!(content: Faker::Lorem.paragraph(sentence_count: rand(3..5)))
+
+2.times do
+  blog_category = Blog::Category.create(
+    title: Faker::Lorem.sentence(word_count: rand(3..5))
+  )
+
+  3.times do
+    blog_category.posts << Blog::Post.create(
+      content:    Faker::Lorem.paragraphs(number: rand(3..15)).join("\n"),
+      title:      Faker::Lorem.sentence,
+      visible_at: Time.zone.now,
+      visible:    true
+    )
+  end
+end
+
+2.times do
+  section = Section.create(
+    description: Faker::Lorem.paragraphs(number: rand(1..2)).join("\n"),
+    name:        Faker::Lorem.sentence(word_count: rand(3..5))
+  )
+
+  3.times do
+    section.items << Item.create(
+      content: Faker::Lorem.paragraphs(number: rand(3..15)).join("\n"),
+      link:    'https://www.lockyy.com',
+      name:    Faker::Lorem.sentence
+    )
+  end
+end
